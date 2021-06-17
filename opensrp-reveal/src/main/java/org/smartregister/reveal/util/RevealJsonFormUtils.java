dda692e27242ad181a5779816b8db56c899fca89
Linda Oscar Gcaba <lindagcaba@gmail.com>
2021-06-17 12:31:48 +0200

    use different "Register_Structure" form for Zambia as per requirements.

--- opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/util/RevealJsonFormUtils.java
@@ -331,6 +331,8 @@ public class RevealJsonFormUtils {
                 formName = JsonForm.THAILAND_ADD_STRUCTURE_FORM;
             } else if (BuildConfig.BUILD_COUNTRY == Country.NAMIBIA) {
                 formName = JsonForm.NAMIBIA_ADD_STRUCTURE_FORM;
+            }else if(BuildConfig.BUILD_COUNTRY == Country.ZAMBIA){
+                formName = JsonForm.ZAMBIA_ADD_STRUCTURE_FORM;
             } else {
                 formName = JsonForm.ADD_STRUCTURE_FORM;
             }
