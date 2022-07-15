eea0c0c5dedac8e639b61147cc8253158ac739f9
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-07-15 16:29:33 +0200

    add failed login ui test.

--- opensrp-reveal/src/androidTest/java/com/revealprecision/data/SampleData.kt
+++ opensrp-reveal/src/androidTest/java/com/revealprecision/data/SampleData.kt
@@ -3,4 +3,6 @@ package com.revealprecision.data
 object SampleData {
     const val VALID_USER_NAME = "reuben.kaponde"
     const val VALID_PASSWORD  = "password"
+    const val INVALID_USER ="does_not_exist"
+    const val INVALID_PASSWORD = "invalid_password"
 }
