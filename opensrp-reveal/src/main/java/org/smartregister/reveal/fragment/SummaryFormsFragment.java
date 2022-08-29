57a2191088b042b8df5dbad5576610ea23cfb18e
Linda Oscar Gcaba <lindagcaba@gmail.com>
2022-08-29 16:01:11 +0200

    do no open "other forms" section when plan and operational ares area not set

--- opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/SummaryFormsFragment.java
+++ opensrp-reveal/src/main/java/org/smartregister/reveal/fragment/SummaryFormsFragment.java
@@ -9,11 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import org.json.JSONObject;
 import org.smartregister.reveal.BuildConfig;
 import org.smartregister.reveal.R;
